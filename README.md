6fbd0be4893da640d1df878963ce7bdbc23859dc
sauves <leonardsauve@gmail.com>
2024-10-30 20:58:32 -0400

    updated all the stuff

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# What is the data?
+![image](https://github.com/user-attachments/assets/924436c4-4044-46c7-92b7-b989a95bb83a)
+The data is all the integers, from 1 to 1000 represented by a binary vector of their prime factors. The values are set to 1 when a the prime is one of the integer's prime factors.  It is set to 0 otherwise. Consequently, many squares are set to 0, and power integers have the exact same binary encoding.
+
+The code necessary to generate this data can be found in the 
+
 # prime_factors_2D
 
 ![image](https://github.com/user-attachments/assets/ea593d27-0324-41bd-b90d-5d0ef85afb31)
@@ -6,3 +12,6 @@ All integers from 1 to 40,000 represented as binary vectors of their prime facto
 
 ![image](https://github.com/user-attachments/assets/924436c4-4044-46c7-92b7-b989a95bb83a)
 All integers from 1 to 80,000 represented as binary vectors of their prime factors divisibility. The representation of the points is done with UMAP. Color of point represent the value (yellow is high, blue is mid, purple is low).
+
+![image](https://github.com/user-attachments/assets/924436c4-4044-46c7-92b7-b989a95bb83a)
+All integers from 1 to 100,000 represented as binary vectors of their prime factors divisibility. The representation of the points is done with UMAP. Color of point represent the value (yellow is high, blue is mid, purple is low).
---
+++ prime_factors.jl
@@ -0,0 +1,57 @@
+function load_prime_factors_data(filename)
+    infile = h5open(filename, "r")
+    MM = infile["data"][:,:]
+    close(infile)
+    return MM
+end 
+
+struct NPrimes{T} P::T end
+NPrimes(N::Integer = 10^8) = NPrimes(primes(N))
+function _nprimes(x::T, P) where T
+    x < P[1] && return zero(T)
+    x == P[end] && return length(P)
+    x > P[end] && return typemax(T)
+    i, j = 1, length(P)
+    while i != j-1
+        k = i + (j - i) ÷ 2
+        if P[k] > x
+            j = k
+        elseif P[k] ≤ x
+            i = k
+        end
+    end
+    return T(i)
+end
+(f::NPrimes)(x) = _nprimes(x, f.P)
+nprimes = NPrimes()
+
+function prime_factor_div(X, PF)
+    return [X % div == 0 for div in PF]
+end 
+
+function load_prime_factors_data(filename)
+    infile = h5open(filename, "r")
+    MM = infile["data"][:,:]
+    close(infile)
+    return MM
+end 
+
+function get_prime_matrix(;n=100_000, outdir = "Data/NUMBERS")
+
+    PF = NPrimes(n).P
+    d = length(PF)
+    # MM = Array{Int64, 2}(undef, (n,d))
+    MM = zeros(n, d)
+    ## 
+    for i in 1:n
+        MM[i,:] = prime_factor_div(i, PF)
+        if i % 10_000 == 0
+            println("completed : $(i * 100 / n)% ")
+        end 
+    end 
+    # save number matrix for later 
+    outfile = h5open("$(outdir)/prime_divisibility_$(n).h5", "w")
+    outfile["data"] = MM
+    close(outfile)
+    return MM, PF
+end 
